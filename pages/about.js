cb1d0f337c0b18c7a7366c864ae4771b6fbc304b
KaroliinaVirtanen <karoliina_261@hotmail.com>
2023-02-20 21:28:14 +0200

    Connecting the App and API together

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function About() {
+    return (
+        <div className='container'>
+            <Head>
+                <title>About us</title>
+            </Head>
+        <h1>About</h1>
+        </div>
+    )
+}
